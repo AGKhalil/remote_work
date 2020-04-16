a7fca2a336ada85a02e1fc96aa5c1127a912fc7c
khela <khela@maat.broadband>
2020-04-16 02:44:39 +0100

    fix bsh update

--- remoteme.sh
+++ remoteme.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash -i
 
 echo 'alias nvimpy="docker run -it --rm -v $(pwd):/data --workdir /data agkhalil/python-neovim:latest"' >> ~/.bashrc
-exec bash
+source ~/.bashrc
